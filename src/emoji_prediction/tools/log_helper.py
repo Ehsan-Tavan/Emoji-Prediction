5c78c677cb492a00099ce57d922f11c23e8c9705
Ehsan-Tavan <ehsantavan98@gmail.com>
2020-10-30 11:28:57 +0330

    change input dictionary's keys

--- src/emoji_prediction/tools/log_helper.py
+++ src/emoji_prediction/tools/log_helper.py
@@ -11,11 +11,11 @@ __author__ = "Ehsan Tavan"
 __organization__ = "Persian Emoji Prediction"
 __credits__ = ["Ehsan Tavan"]
 __license__ = "Public Domain"
-__version__ = "1.1.1"
+__version__ = "1.2.0"
 __maintainer__ = "Ehsan Tavan"
 __email__ = "tavan.ehsan@gmail.com"
 __status__ = "Production"
-__date__ = "10/09/2020"
+__date__ = "10/30/2020"
 
 logging.basicConfig(
     format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO)
@@ -54,27 +54,27 @@ def model_result_log(train_log_dict, valid_log_dict, test_log_dict):
     :param valid_log_dict: dictionary of validation data result
     :param test_log_dict: dictionary of test data result
     """
-    logging.info(f"\tTrain. Loss: {train_log_dict['train_loss']:.4f} | "
-                 f"Train. Acc: {train_log_dict['train_acc'] * 100:.2f}%")
-    logging.info(f"\t Val. Loss: {valid_log_dict['valid_loss']:.4f} |  "
-                 f"Val. Acc: {valid_log_dict['valid_acc'] * 100:.2f}%")
-    logging.info(f"\t Test. Loss: {test_log_dict['test_loss']:.4f} |  "
-                 f"Test. Acc: {test_log_dict['test_acc'] * 100:.2f}%")
+    logging.info(f"\tTrain. Loss: {train_log_dict['loss']:.4f} | "
+                 f"Train. Acc: {train_log_dict['acc'] * 100:.2f}%")
+    logging.info(f"\t Val. Loss: {valid_log_dict['loss']:.4f} |  "
+                 f"Val. Acc: {valid_log_dict['acc'] * 100:.2f}%")
+    logging.info(f"\t Test. Loss: {test_log_dict['loss']:.4f} |  "
+                 f"Test. Acc: {test_log_dict['acc'] * 100:.2f}%")
 
-    logging.info(f"\t Train. Precision: {train_log_dict['train_precision']}")
-    logging.info(f"\t Train. Recall: {train_log_dict['train_recall']}")
-    logging.info(f"\t Train. F1_Score: {train_log_dict['train_fscore']}")
-    logging.info(f"\t Train. Total F1 score: {train_log_dict['train_total_fscore']}")
+    logging.info(f"\t Train. Precision: {train_log_dict['precision']}")
+    logging.info(f"\t Train. Recall: {train_log_dict['recall']}")
+    logging.info(f"\t Train. F1_Score: {train_log_dict['f-score']}")
+    logging.info(f"\t Train. Total F1 score: {train_log_dict['total_fscore']}")
 
-    logging.info(f"\t Val. Precision: {valid_log_dict['valid_precision']}")
-    logging.info(f"\t Val. Recall: {valid_log_dict['valid_recall']}")
-    logging.info(f"\t Val. F1_Score: {valid_log_dict['valid_fscore']}")
-    logging.info(f"\t Val. Total F1 score: {valid_log_dict['valid_total_fscore']}")
+    logging.info(f"\t Val. Precision: {valid_log_dict['precision']}")
+    logging.info(f"\t Val. Recall: {valid_log_dict['recall']}")
+    logging.info(f"\t Val. F1_Score: {valid_log_dict['f-score']}")
+    logging.info(f"\t Val. Total F1 score: {valid_log_dict['total_fscore']}")
 
-    logging.info(f"\t Test. Precision: {test_log_dict['test_precision']}")
-    logging.info(f"\t Test. Recall: {test_log_dict['test_recall']}")
-    logging.info(f"\t Test. F1_Score: {test_log_dict['test_fscore']}")
-    logging.info(f"\t Test. Total F1 score: {test_log_dict['test_total_fscore']}")
+    logging.info(f"\t Test. Precision: {test_log_dict['precision']}")
+    logging.info(f"\t Test. Recall: {test_log_dict['recall']}")
+    logging.info(f"\t Test. F1_Score: {test_log_dict['f-score']}")
+    logging.info(f"\t Test. Total F1 score: {test_log_dict['total_fscore']}")
 
     logging.info("_____________________________________________________________________\n")
 
@@ -82,33 +82,33 @@ def model_result_log(train_log_dict, valid_log_dict, test_log_dict):
 def model_result_save(log_file, train_log_dict, valid_log_dict, test_log_dict):
     """
     model_result_save method is writen for save model result on each epoch
+    :param log_file: text log file
     :param train_log_dict: dictionary of train data result
     :param valid_log_dict: dictionary of validation data result
     :param test_log_dict: dictionary of test data result
-    :param human_test_log_dict: dictionary of human test data result
     """
     # log_file.write(f"Epoch: {epoch + 1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s\n")
-    log_file.write(f"\tTrain Loss: {train_log_dict['train_loss']:.4f} | "
-                   f"Train Acc: {train_log_dict['train_acc'] * 100:.2f}%\n")
-    log_file.write(f"\t Val. Loss: {valid_log_dict['valid_loss']:.4f} |  "
-                   f"Val. Acc: {valid_log_dict['valid_acc'] * 100:.2f}%\n")
-    log_file.write(f"\t Test. Loss: {test_log_dict['test_loss']:.4f} |  "
-                   f"Test. Acc: {test_log_dict['test_acc'] * 100:.2f}%\n")
+    log_file.write(f"\tTrain Loss: {train_log_dict['loss']:.4f} | "
+                   f"Train Acc: {train_log_dict['acc'] * 100:.2f}%\n")
+    log_file.write(f"\t Val. Loss: {valid_log_dict['loss']:.4f} |  "
+                   f"Val. Acc: {valid_log_dict['acc'] * 100:.2f}%\n")
+    log_file.write(f"\t Test. Loss: {test_log_dict['loss']:.4f} |  "
+                   f"Test. Acc: {test_log_dict['acc'] * 100:.2f}%\n")
 
-    log_file.write(f"\t Train. Precision: {train_log_dict['train_precision']}\n")
-    log_file.write(f"\t Train. Recall: {train_log_dict['train_recall']}\n")
-    log_file.write(f"\t Train. F1_Score: {train_log_dict['train_fscore']}\n")
-    log_file.write(f"\t Train. Total F1 score: {train_log_dict['train_total_fscore']}\n")
+    log_file.write(f"\t Train. Precision: {train_log_dict['precision']}\n")
+    log_file.write(f"\t Train. Recall: {train_log_dict['recall']}\n")
+    log_file.write(f"\t Train. F1_Score: {train_log_dict['f-score']}\n")
+    log_file.write(f"\t Train. Total F1 score: {train_log_dict['total_fscore']}\n")
 
-    log_file.write(f"\t Val. Precision: {valid_log_dict['valid_precision']}\n")
-    log_file.write(f"\t Val. Recall: {valid_log_dict['valid_recall']}\n")
-    log_file.write(f"\t Val. F1_Score: {valid_log_dict['valid_fscore']}\n")
-    log_file.write(f"\t Val. Total F1 score: {valid_log_dict['valid_total_fscore']}\n")
+    log_file.write(f"\t Val. Precision: {valid_log_dict['precision']}\n")
+    log_file.write(f"\t Val. Recall: {valid_log_dict['recall']}\n")
+    log_file.write(f"\t Val. F1_Score: {valid_log_dict['f-score']}\n")
+    log_file.write(f"\t Val. Total F1 score: {valid_log_dict['total_fscore']}\n")
 
-    log_file.write(f"\t Test. Precision: {test_log_dict['test_precision']}\n")
-    log_file.write(f"\t Test. Recall: {test_log_dict['test_recall']}\n")
-    log_file.write(f"\t Test. F1_Score: {test_log_dict['test_fscore']}\n")
-    log_file.write(f"\t Test. Total F1 score: {test_log_dict['test_total_fscore']}\n")
+    log_file.write(f"\t Test. Precision: {test_log_dict['precision']}\n")
+    log_file.write(f"\t Test. Recall: {test_log_dict['recall']}\n")
+    log_file.write(f"\t Test. F1_Score: {test_log_dict['f-score']}\n")
+    log_file.write(f"\t Test. Total F1 score: {test_log_dict['total_fscore']}\n")
 
     log_file.write("____________________________________________________________\n")
     log_file.flush()
