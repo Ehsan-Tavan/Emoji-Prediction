6fd4cb59dd01d387f6c2a12b704dc068c4eb26ab
Ehsan-Tavan <tavan.ehsan@gmail.com>
2020-11-19 16:47:22 +0330

    add to(DEVICE) for solve gpu error

--- src/emoji_prediction/tools/evaluation_helper.py
+++ src/emoji_prediction/tools/evaluation_helper.py
@@ -7,16 +7,17 @@ evaluation_helper.py is a evaluation file for writing evaluation methods
 """
 
 import torch
+from emoji_prediction.config.cnn_config import DEVICE
 
 __author__ = "Ehsan Tavan"
 __organization__ = "Persian Emoji Prediction"
 __credits__ = ["Ehsan Tavan"]
 __license__ = "Public Domain"
-__version__ = "1.0.1"
+__version__ = "1.0.0"
 __maintainer__ = "Ehsan Tavan"
 __email__ = "tavan.ehsan@gmail.com"
 __status__ = "Production"
-__date__ = "10/30/2020"
+__date__ = "11/19/2020"
 
 
 def binary_accuracy(preds, target):
@@ -36,4 +37,4 @@ def categorical_accuracy(preds, target):
     """
     max_preds = preds.argmax(dim=1, keepdim=True)   # get the index of the max probability
     correct = max_preds.squeeze(1).eq(target)
-    return correct.sum() / torch.FloatTensor([target.shape[0]])
+    return correct.sum().to(DEVICE) / torch.FloatTensor([target.shape[0]]).to(DEVICE)
