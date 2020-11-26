6edd72a0710e251433126919fca677061b117118
Ehsan-Tavan <tavan.ehsan@gmail.com>
2020-11-26 20:59:20 +0330

    initial commit

---
+++ src/emoji_prediction/config/lstm_cnn_config.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# pylint: disable-msg=no-member
+
+"""
+lstm_cnn_config.py is written for LstmCnn model
+"""
+
+import torch
+
+__author__ = "Ehsan Tavan"
+__project__ = "Persian Emoji Prediction"
+__credits__ = ["Ehsan Tavan"]
+__license__ = "Public Domain"
+__version__ = "1.0.0"
+__maintainer__ = "Ehsan Tavan"
+__email__ = "tavan.ehsan@gmail.com"
+__status__ = "Production"
+__date__ = "11/26/2020"
+
+
+RAW_NO_MENTION_DATA_PATH = "../data/Raw/first_categori_no_mention_tweets.csv"
+RAW_DATA_PATH = "../data/Raw/first_categori_tweets.csv"
+TRAIN_NORMAL_NO_MENTION_DATA_PATH = "../data/Processed/" \
+                                    "train_first_categori_no_mention_tweets_normal.csv"
+TEST_NORMAL_NO_MENTION_DATA_PATH = "../data/Processed/" \
+                                   "test_first_categori_no_mention_tweets_normal.csv"
+TRAIN_NORMAL_DATA_PATH = "../data/Processed/" \
+                                    "train_first_categori_tweets_normal_5.csv"
+TEST_NORMAL_DATA_PATH = "../data/Processed/" \
+                                   "train_first_categori_tweets_normal_5.csv"
+VALIDATION_NORMAL_DATA_PATH = "../data/Processed/" \
+                                   "train_first_categori_tweets_normal_5.csv"
+
+GLOVE_NEWS_300D = "../data/Embeddings/news_glove_300d_e10.txt"
+SKIPGRAM_NEWS_300D = "../data/Embeddings/skipgram_news_300d_30e.txt"
+CBOW_NEWS_300D = "../data/Embeddings/cbow_news_300d_30e.txt"
+
+LOSS_CURVE_PATH = "../models/tmp/Curves/loss_curve.png"
+ACC_CURVE_PATH = "../models/tmp/Curves/accuracy_curve.png"
+
+TEXT_FIELD_PATH = "../models/tmp/Fields/text_field"
+LABEL_FIELD_PATH = "../models/tmp/Fields/label_field"
+LOG_PATH = "../models/tmp/Logs/log.txt"
+MODEL_PATH = "../models/tmp/"
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+N_EPOCHS = 30
+BATCH_SIZE = 256
+EMBEDDING_DIM = 300
+LSTM_HIDDEN_DIM = 500
+BIDIRECTIONAL = True
+FILTER_SIZE = [3, 4, 5]
+N_FILTERS = 256
+START_DROPOUT = 0.1
+MIDDLE_DROPOUT = 0.2
+FINAL_DROPOUT = 0.3
